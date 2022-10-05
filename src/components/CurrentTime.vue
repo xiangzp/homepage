d643d03d765e09abcb3d0364df99d70c9c0b6834
zpxiang <zpxiang@iflytek.com>
2022-10-05 22:40:10 +0800

    init

--- src/components/CurrentTime.vue
+++ src/components/CurrentTime.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="current-time">
     <div class="time">{{ currentTime }}</div>
-    <div class="day">{{ currentDay }}</div>
+    <div class="day">
+      {{ currentDay }}<span style="margin-left: 10px">{{ week }}</span>
+    </div>
   </div>
 </template>
 
@@ -10,13 +12,44 @@ import dayjs from "dayjs";
 import { ref, onUnmounted } from "vue";
 
 const currentTime = ref(dayjs().format("hh:mm:ss"));
-const currentDay = ref(dayjs().format("MM/DD"));
-
+const currentDay = ref();
+const week = ref();
 let timer;
+
+function getWeek() {
+  let day = dayjs().day();
+  switch (day) {
+    case 1:
+      day = "一";
+      break;
+    case 2:
+      day = "二";
+      break;
+    case 3:
+      day = "三";
+      break;
+    case 4:
+      day = "四";
+      break;
+    case 5:
+      day = "五";
+      break;
+    case 6:
+      day = "六";
+      break;
+    case 0:
+      day = "日";
+      break;
+  }
+  return day;
+}
+
 function cal() {
   clearTimeout(timer);
+  currentTime.value = dayjs().format("hh:mm:ss");
+  currentDay.value = dayjs().format("MM/DD");
+  week.value = `周${getWeek()}`;
   timer = setTimeout(() => {
-    currentTime.value = dayjs().format("hh:mm:ss");
     cal();
   }, 1000);
 }

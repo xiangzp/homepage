abadf94b4dbeb679ea9fd35a50aedbddbcd36dfe
zpxiang <zpxiang@iflytek.com>
2022-10-07 16:06:43 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import CurrentTime from "./components/CurrentTime.vue";
 import { request } from "./utils/request";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import { Plus } from "@element-plus/icons-vue";
+import NewsList from "./components/NewsList.vue";
 
 const navigates = ref([]);
 request.get("https://78bnit.lafyun.com:443/get-navigates").then((res) => {
@@ -33,6 +35,34 @@ request.get("https://78bnit.lafyun.com:443/get-weather").then((res) => {
     weather.value = {};
   }
 });
+
+const dialogVisible = ref(false);
+const formRef = ref();
+const formModal = ref({
+  icon: "",
+  name: "",
+  path: "",
+});
+const formRules = reactive({
+  icon: [{ required: true, message: "请填写图标地址" }],
+  name: [{ required: true, message: "请填写名称" }],
+  path: [{ required: true, message: "请填写导航地址" }],
+});
+
+function addNewNavigate() {
+  dialogVisible.value = true;
+}
+
+function submitNewNavigate() {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      request.post(
+        "https://78bnit.lafyun.com:443/add-navigate",
+        formModal.value
+      );
+    }
+  });
+}
 </script>
 
 <template>
@@ -55,12 +85,47 @@ request.get("https://78bnit.lafyun.com:443/get-weather").then((res) => {
           </div>
           <p>{{ url.name }}</p>
         </a>
+        <a class="url" @click="addNewNavigate">
+          <div class="icon add-plus">
+            <el-icon :size="24">
+              <Plus />
+            </el-icon>
+          </div>
+        </a>
       </div>
     </div>
+    <news-list class="new-list" />
   </div>
+  <el-dialog v-model="dialogVisible" width="500px" title="添加导航网址">
+    <el-form
+      ref="formRef"
+      :model="formModal"
+      label-width="100px"
+      :rules="formRules"
+    >
+      <el-form-item label="网址名称" prop="name">
+        <el-input placeholder="请输入网址名称" v-model="formModal.name" />
+      </el-form-item>
+      <el-form-item label="导航路径" prop="path">
+        <el-input placeholder="请输入导航路径" v-model="formModal.path" />
+      </el-form-item>
+      <el-form-item label="图标地址" prop="icon">
+        <el-input
+          placeholder="请输入图标地址，如http://xxxx"
+          v-model="formModal.icon"
+        />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitNewNavigate">提交</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 .bg {
   position: fixed;
   top: 0;
@@ -75,9 +140,11 @@ request.get("https://78bnit.lafyun.com:443/get-weather").then((res) => {
 }
 
 .homepage {
-  width: 90%;
-  max-width: 1600px;
-  margin: 120px auto 0;
+  position: fixed;
+  top: 100px;
+  left: 20px;
+  bottom: 20px;
+  right: 380px;
 }
 
 .navigate + .navigate {
@@ -96,8 +163,40 @@ request.get("https://78bnit.lafyun.com:443/get-weather").then((res) => {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   transition: transform 0.3s;
+
+  .icon {
+    width: 100px;
+    height: 100px;
+    border-radius: 6px;
+    background: #fff;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    justify-content: center;
+  }
+
+  .add-plus {
+    background: transparent;
+    border: 1px dashed #d8d8d8;
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+    display: block;
+  }
+
+  p {
+    line-height: 1;
+    margin-top: 12px;
+    font-size: 12px;
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
 }
 
 .group-name {
@@ -109,36 +208,19 @@ request.get("https://78bnit.lafyun.com:443/get-weather").then((res) => {
   color: transparent;
 }
 
-.url .icon {
-  width: 100px;
-  height: 100px;
-  border-radius: 6px;
-  background: #fff;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-}
-
-.url img {
-  width: 100%;
-  display: block;
-}
-
-.url p {
-  line-height: 1;
-  margin-top: 12px;
-  font-size: 12px;
-}
-
-.url:hover {
-  transform: scale(1.1);
-}
-
 .weather {
   position: fixed;
   top: 20px;
   left: 20px;
   color: #e8e8e8;
   font-size: 12px;
+}
+
+.new-list {
+  position: fixed;
+  right: 20px;
+  top: 20px;
+  bottom: 20px;
+  width: 350px;
 }
 </style>
---
+++ src/components/NewsList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="card">
+      <template v-if="news">
+        <div style="font-size: 22px; margin-bottom: 20px">{{ news.name }}</div>
+        <div v-for="link in news.list" :key="link.title" class="link">
+          <a target="_blank" :href="link.link">
+            <el-icon>
+              <Link />
+            </el-icon>
+            {{ link.title }}
+          </a>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { request } from "../utils/request";
+import { Link } from "@element-plus/icons-vue";
+
+const news = ref();
+
+request
+  .get("https://78bnit.lafyun.com:443/get-news", {
+    params: {
+      type: "weibo",
+    },
+  })
+  .then((res) => {
+    news.value = res.data.data;
+  });
+</script>
+
+<style scoped lang="less">
+.card {
+  background: rgba(#000, 0.8);
+  border-radius: 12px;
+  padding: 24px;
+  box-shadow: 0 0 10px rgba(#000, 0.2);
+  height: 100%;
+  color: #fff;
+  box-sizing: border-box;
+  overflow: hidden;
+
+  &:hover {
+    overflow: auto;
+  }
+
+  .link {
+    font-size: 14px;
+    transition: all 0.3s;
+    a {
+      color: #aaa;
+      text-decoration: none;
+      font-size: 16px;
+      transition: color 0.3s;
+    }
+    &:hover {
+      a {
+        color: #fff;
+      }
+    }
+  }
+
+  .link + .link {
+    margin-top: 8px;
+  }
+}
+</style>
